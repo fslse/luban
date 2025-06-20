f07ffef8593055fbf5ccd6b632f6fb6d6542304a
fslse <3455403891@qq.com>
2025-06-20 17:38:48 +0800

    新增 cs-unitask-bin 代码生成目标

---
+++ src/Luban.CSharp/CodeTarget/CsharpUniTaskBinTarget.cs
@@ -0,0 +1,15 @@
+﻿using Luban.CodeTarget;
+using Luban.CSharp.TemplateExtensions;
+using Scriban;
+
+namespace Luban.CSharp.CodeTarget;
+
+[CodeTarget("cs-unitask-bin")]
+public class CsharpUniTaskBinCodeTarget : CsharpCodeTargetBase
+{
+    protected override void OnCreateTemplateContext(TemplateContext ctx)
+    {
+        base.OnCreateTemplateContext(ctx);
+        ctx.PushGlobal(new CsharpBinTemplateExtension());
+    }
+}
---
+++ src/Luban.CSharp/TemplateExtensions/CsharpUniTaskBinTemplateExtension.cs
@@ -0,0 +1,13 @@
+﻿using Luban.CSharp.TypeVisitors;
+using Luban.Types;
+using Scriban.Runtime;
+
+namespace Luban.CSharp.TemplateExtensions;
+
+public class CsharpUniTaskBinTemplateExtension : ScriptObject
+{
+    public static string Deserialize(string bufName, string fieldName, TType type)
+    {
+        return type.Apply(BinaryDeserializeVisitor.Ins, bufName, fieldName);
+    }
+}
